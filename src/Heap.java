18af29037e2778d8e18735f5ae54d8eb8382cd9e
Sarmad <engrsarmadchandio@gmail.com>
2025-05-03 15:36:25 +0500

    main method commit

--- src/Heap.java
+++ src/Heap.java
@@ -28,7 +28,7 @@ public class Heap {
     private int getParent(int index){
         //if we want to make 0 index open formula will be parent = index/2;
         //if we don't , want to make 0 index open. formula will be ent = index-1/2;
-        return index-1/2 ;
+        return (index-1)/2 ;
     }
 
     private void swap(int index1, int index2){
@@ -41,12 +41,57 @@ public class Heap {
         heap.add(value);
         int current = heap.size() - 1;
 
-        while (current>=0 &&
+        while (current>0 &&
                 heap.get(current) > heap.get(getParent(current))){
             swap(current,getParent(current));
             current = getParent(current);
         }
     }
+
+    //by not using sink down
+    public Integer remove() {
+        if (heap.isEmpty()) return null;
+        if (heap.size() == 1) {
+            return heap.remove(0);
+        }
+        int removedValue = heap.get(0);
+        int lastValue = heap.remove(heap.size() - 1); // Remove last element
+        heap.set(0, lastValue); // Replace root with last element
+        int current = 0;
+        while (true) {
+            int left = leftChild(current);
+            int right = rightChild(current);
+            int largest = current;
+
+            if (left < heap.size() && heap.get(left) > heap.get(largest)) {
+                largest = left;
+            }
+            if (right < heap.size() && heap.get(right) > heap.get(largest)) {
+                largest = right;
+            }
+            if (largest != current) {
+                swap(current, largest);
+                current = largest;
+            } else {
+                break;
+            }
+        }
+        return removedValue;
+    }
+
+
+//    // by using sink down
+//    private void sinkDown(int index){
+//        int maxIndex = index;
+//        while(true){
+//            int leftIndex = leftChild(index);
+//            int rightIndex = rightChild(index);
+//
+//            if (leftIndex < heap.size() && heap.g)
+//        }
+//
+//    }
+
 
 
 
